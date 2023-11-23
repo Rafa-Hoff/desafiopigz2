ae1035430c7d9c9e2fd6f088f52f783084e5d62c
Rafa-Hoff <rafahoff32@gmail.com>
2023-11-22 21:09:34 -0400

    Criado LoginController com metodos Criar e Deletar usuario

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Usuario;
+use App\Repository\UsuarioRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/usuario/criar', name: 'usuario_criar')]
+    public function createUsuario(Request $request, UsuarioRepository $usuarioRepository): JsonResponse
+    {
+        $data = $request->request->all();
+
+        $usuario = new Usuario();
+        
+        $usuario->setEmail($data['email']);
+        $usuario->setPassword($data['senha']);
+        $usuario->setRoles([$data['role']]);
+
+        $usuarioRepository->add($usuario);
+        
+
+        return $this->json([
+            'message' => 'Usuario criado com sucesso',
+            'data' => $usuario->getEmail()
+        ]);
+    }
+
+    #[Route('/usuario/deletar', name: 'usuario_delete')]
+    public function deleteUsuario(int $id, UsuarioRepository $usuarioRepository): JsonResponse
+    {
+        $usuario = $usuarioRepository->find($id);
+        $usuario = $usuarioRepository->remove($usuario, true);
+
+        return $this->json([
+            'message' => 'Usuario deletado com sucesso'
+        ]);
+    }
+}
